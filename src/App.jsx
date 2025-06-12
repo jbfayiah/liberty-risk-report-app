0d151c8464a4438eb069bdd7546fea0083e5a0b1
James Fayiah <james@perilmavensl.com>
2025-06-12 04:13:16 -0500

    Fixed file visibility and updated form

--- src/App.jsx
+++ src/App.jsx
@@ -177,3 +177,11 @@ function LibertyRiskForm() {
 }
 
 export default LibertyRiskForm;
+import LibertyRiskForm from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <LibertyRiskForm />
+  </React.StrictMode>
+);
